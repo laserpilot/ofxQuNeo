70496c3c8e48b2f2e3b62fdbaebb1c08691d3b5a
laserpilot <blair@fakelove.tv>
2014-06-04 14:03:06 -0400

    refactor for OF addon format

---
+++ BasicExample/src/testApp.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2013 Dan Wilcox <danomatika@gmail.com>
+ *
+ * BSD Simplified License.
+ * For information on usage and redistribution, and for a DISCLAIMER OF ALL
+ * WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+ *
+ * See https://github.com/danomatika/ofxMidi for documentation
+ *
+ */
+#pragma once
+
+#include "ofMain.h"
+
+#include "ofxMidi.h"
+
+#include "ofxQuNeo.h"
+
+#include "ofxMPD24.h"
+
+class testApp : public ofBaseApp {
+	
+public:
+	
+	void setup();
+	void update();
+	void draw();
+	void exit();
+	
+	void keyPressed(int key);
+	void keyReleased(int key);
+	
+	void mouseMoved(int x, int y );
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased();
+	
+    
+    ofxQuNeo quNeo;
+    ofxMPD24 mpd24;
+    
+};
